7432aec4f4f7af779682677df00aa4aad1e921c4
claxn <clax.n@gmx.at>
2019-05-20 10:46:50 +0200

    Initial commit

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+============
+Contributors
+============
+
+* claxn <clax.n@gmx.at>
---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from yeoda.skeleton import fib
+
+__author__ = "claxn"
+__copyright__ = "claxn"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)

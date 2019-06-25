f3a8189a55bab860a6c2c1cfaa55ca1420077cc1
Bernhard Bauer-Marschallinger <bernhard.bauer-marschallinger@geo.tuwien.ac.at>
2019-06-25 15:03:57 +0200

    added unittest structure

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -1,4 +1,7 @@
 # general imports
+import os
+import unittest
+
 from shapely.wkt import load
 from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
@@ -12,16 +15,42 @@ from geopathfinder.sgrt_naming import create_sgrt_filename, sgrt_tree
 # import grid
 from Equi7Grid.equi7grid.equi7grid import Equi7Grid
 
-#root_dirpath = r"R:\Projects\SHRED\07_data\Sentinel-1_CSAR"
-root_dirpath = r"D:\work\data\yeoda\Sentinel-1_CSAR"
-roi = Polygon([(4373136, 1995726), (4373136, 3221041), (6311254, 3221041), (6311254, 1995726)])
-st = sgrt_tree(root_dirpath, register_file_pattern=(".tif$"))
-eodc = eoDataCube(dir_tree=st, smart_filename_creator=create_sgrt_filename,
-                  dimensions=['time', 'tile_name', 'pol'])
+
+def cur_path():
+    pth, _ = os.path.split(os.path.abspath(__file__))
+    return pth
 
 
-fig, ax = plt.subplots(1, 1)
-eodc.inventory.drop_duplicates(subset=['tile_name']).plot(ax=ax, edgecolor='red', facecolor="none")
-eodc_roi = eodc.filter_spatially(roi=roi)
-eodc_roi.inventory.drop_duplicates(subset=['tile_name']).plot(ax=ax, edgecolor='blue', facecolor="none")
-plt.show()
+
+class TestEODataCube(unittest.TestCase):
+
+
+    def setUp(self):
+        self.path = os.path.join(cur_path(), 'testdata')
+
+
+    def tearDown(self):
+
+        pass
+
+
+    def test_filter_spatially(self):
+
+        root_dirpath = os.path.join(self.path, 'Sentinel-1_CSAR')
+        roi = Polygon([(4373136, 1995726), (4373136, 3221041), (6311254, 3221041), (6311254, 1995726)])
+        st = sgrt_tree(root_dirpath, register_file_pattern=(".tif$"))
+        eodc = eoDataCube(dir_tree=st, smart_filename_creator=create_sgrt_filename,
+                          dimensions=['time', 'tile_name', 'pol'])
+
+
+        fig, ax = plt.subplots(1, 1)
+
+        eodc.inventory.drop_duplicates(subset=['tile_name']).plot(ax=ax, edgecolor='red', facecolor="none")
+        eodc_roi = eodc.filter_spatially(roi=roi)
+        eodc_roi.inventory.drop_duplicates(subset=['tile_name']).plot(ax=ax, edgecolor='blue', facecolor="none")
+        plt.show()
+
+        plt.close()
+        fig, ax = None, None
+
+        pass
